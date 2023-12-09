b0226314ae6e8797d475af58f1e80af2fc621bc9
marvinkn <marvin.nyingi97@gmail.com>
2023-12-09 11:59:42 +0300

    test steps

--- src/main/java/dev/marvin/savings/customer/Customer.java
+++ src/main/java/dev/marvin/savings/customer/Customer.java
@@ -1,13 +1,20 @@
 package dev.marvin.savings.customer;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Customer {
+public class Customer implements Serializable {
     private Integer id;
     private String name;
     private String email;
     private Integer mobile;
     private Integer governmentId;
+    private String memberNumber;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public Customer() {
+    }
+
+
 }
