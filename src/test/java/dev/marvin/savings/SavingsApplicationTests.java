6a890a02984a23cafea40c8fbb02e400e09330be
marvinkn <marvin.nyingi97@gmail.com>
2023-12-09 10:46:01 +0300

    getting started

---
+++ src/test/java/dev/marvin/savings/SavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marvin.savings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
