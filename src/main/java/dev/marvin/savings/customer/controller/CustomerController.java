54b1113e9d66894f2b28cb635f71675a3540df14
marvinkn <marvin.nyingi97@gmail.com>
2024-03-05 13:47:36 +0300

    added initial documentation for customer controller

--- src/main/java/dev/marvin/savings/customer/controller/CustomerController.java
+++ src/main/java/dev/marvin/savings/customer/controller/CustomerController.java
@@ -4,6 +4,9 @@ import dev.marvin.savings.customer.dto.CustomerRegistrationRequest;
 import dev.marvin.savings.customer.dto.CustomerResponse;
 import dev.marvin.savings.customer.dto.CustomerUpdateRequest;
 import dev.marvin.savings.customer.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +23,13 @@ import java.util.Optional;
 @RequestMapping(value = "api/v1/customers")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 5, maxFileSize = 1024 * 1024 * 10)
 @RequiredArgsConstructor
+@Tag(name = "Customer Resource", description = "CRUD REST APIs for Customer Resource - Create Customer,Get All Customers, Get Customer, Update Customer,  Delete Customer")
 public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping(path = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponse(responseCode = "201", description = "Customer successfully registered")
+    @Operation(summary = "Register Customer", description = "Register customer is used to save customer in database")
     public ResponseEntity<String> registerCustomer(@Valid @RequestBody CustomerRegistrationRequest registrationRequest) {
         String response = customerService.registerCustomer(registrationRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
--- src/main/java/dev/marvin/savings/customer/dto/CustomerRegistrationRequest.java
+++ src/main/java/dev/marvin/savings/customer/dto/CustomerRegistrationRequest.java
@@ -1,18 +1,25 @@
 package dev.marvin.savings.customer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
+@Schema(name = "Customer Registration Request Model")
 public record CustomerRegistrationRequest(
         @NotBlank(message = "customer name is required")
-        @Size(min = 6, message = "Minimum length is 6")
+        @Min(value = 6, message = "Minimum length is 6 characters")
+        @Max(value = 50, message = "Maximum length is 50 characters")
+        @Schema(name = "name", defaultValue = "Foo Bar")
         String name,
 
         @Email(message = "customer email is invalid")
         @NotBlank(message = "email is required")
+        @Schema(name = "email", defaultValue = "foo@example.com")
         String email,
 
         @NotBlank(message = "password is required")
+        @Schema(name = "password", defaultValue = "password")
         String password) {
 }
