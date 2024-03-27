63e5ebd2017cbd71cb804e9fd812f447de9624ec
marvinkn <marvin.nyingi97@gmail.com>
2024-03-27 14:16:43 +0300

    getting started on jwt security impl

---
+++ src/main/java/dev/marvin/savings/config/JwtAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package dev.marvin.savings.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //you can also implement Filter Interface for this use case;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authenticationHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authenticationHeader == null || !authenticationHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authenticationHeader.substring(7);
+    }
+}
